0efd710d5871e359b5b9d26d471e425c925cfa00
Sonjieun2 <jeun2e2@gmail.com>
2026-06-03 02:24:24 +0900

    날짜 선택 구현(달력 디자인前)

--- src/page/Registration.jsx
+++ src/page/Registration.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import { DatePicker } from 'react-datepicker'
 import { PiStarFill, PiStarLight } from 'react-icons/pi'
+import "react-datepicker/dist/react-datepicker.css"   // 기본 스타일
 import Layout from '../components/common/Layout'
 import Dropdown from '../components/common/Dropdown'
 
@@ -10,9 +12,17 @@ export default function Registration() {
     { title: '애니메이션', id: 'animation' },
   ]
 
+  const Status = [
+    { label: '시청완료', id: 0 },
+    { label: '시청중', id: 1 },
+    { label: '보고싶음', id:2 },
+  ]
+
   const labelClass = "w-24 text-right shrink-0"
   const [chooseCartegory, setChooseCartegory] = useState(null)
   const [rating, setRating] = useState(0)
+  const [chooseStatus, setChooseStatus] = useState(0)
+  const [chooseDate, setChooseDate] = useState(null)
 
   return (
     <Layout>
@@ -65,6 +75,39 @@ export default function Registration() {
                   )
                 )}
               </div>
+            </div>
+
+            {/* 상태 */}
+            <div className="flex flex-row items-center gap-3">
+              <p className={labelClass}>상태</p>  
+              <div className="flex flex-row gap-2">
+                {Status.map((menu) => (
+                  <button
+                    onClick={() => setChooseStatus(menu.id)}
+                    className={`
+                      w-[120px] py-2 border border-beige-700 rounded-lg
+                      ${chooseStatus===menu.id ? 'bg-beige-600' : 'bg-beige-400'}  
+                    `}
+                  >
+                    {menu.label}
+                  </button>
+                ))}
+              </div>
+            </div>
+
+            {/* 날짜 */}
+            <div className="flex flex-row items-center gap-3">
+              <p className={labelClass}>날짜</p>
+              <DatePicker
+                selected={chooseDate}
+                onChange={(date) => setChooseDate(date)}
+                dateFormat="yyyy-MM-dd"
+                disabled={chooseStatus !== 0}
+                className="
+                  w-[250px] px-5 py-2 bg-beige-600 border border-beige-700 rounded-lg
+                  disabled:opacity-50 disabled:cursor-not-allowed
+                "
+              />
             </div>
           </div>
         </div>
